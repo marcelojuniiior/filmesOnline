25a348a8ccc4ff0ea3243e5cac55f573ee53bc44
marcelojuniiior <marcelojuniiior@gmail.com>
2022-10-05 00:23:27 -0300

    atualização2

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.contato', { duration: 1500 });
+sr.reveal('.Scroll', { duration: 1500 });
 
-window.addEventListener('resize', function(){
-    var widht = window.innerWidth
-
-    if( widht < 580){
-        sr.reveal('.melhores', { duration: 1500 });
-    }
-});
